1e95f5e5612234fafb5850a11bd549acf1ef9498
labibahmed10 <labib.ahmed.372@gmail.com>
2024-06-19 12:17:57 +0600

    feat: added react-webcam, done the initial home page draft design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import Webcam from "react-webcam";
+
 function App() {
   return (
     <>
-      <h1>Code is running</h1>
+      <section className="flex items-center justify-center h-screen bg-[#03285D]">
+        <div className="py-[2rem] px-[5rem] bg-slate-50 flex flex-col items-center justify-center gap-8">
+          <h1 className="text-4xl text-blue-600/80">Take Selfie</h1>
+
+          <Webcam />
+
+          <button className="bg-amber-500 px-10 py-2 text-lg font-semibold text-white uppercase leading-2">Continute</button>
+        </div>
+      </section>
     </>
   );
 }
