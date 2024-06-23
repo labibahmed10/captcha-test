f9787a700b85f175a0b41e43684ff19df2be81e3
labibahmed10 <labib.ahmed.372@gmail.com>
2024-06-23 16:52:21 +0600

    chore: user logic changed in block count down page

--- src/components/BlockedError.tsx
+++ src/components/BlockedError.tsx
@@ -1,35 +1,9 @@
-import { forwardRef, MutableRefObject, useEffect, useRef, useState } from "react";
+import { forwardRef } from "react";
 import { IBlockedErrorProps } from "../types";
+import useBlockCountdown from "../helper/useBlockCountdown";
 
 const BlockedError = forwardRef<number | undefined, IBlockedErrorProps>((props, ref) => {
-  const { setNumOfWrongValidation, setImageSrc, startInterval, setSquareShapePostion, setAllCaptchaSquareBoxs } = props;
-
-  const [timeLeft, setTimeLeft] = useState(3600);
-  const timeLeftRef = useRef<number | undefined>();
-
-  useEffect(() => {
-    timeLeftRef.current = setInterval(() => {
-      setTimeLeft((prevSeconds) => {
-        if (prevSeconds <= 0) {
-          clearInterval(timeLeftRef.current);
-          return 0;
-        }
-        return prevSeconds - 1;
-      });
-    }, 1000);
-
-    if (timeLeft === 0) {
-      setNumOfWrongValidation(0);
-      setImageSrc(null);
-      (ref as MutableRefObject<number | undefined>).current = startInterval(setSquareShapePostion);
-      setAllCaptchaSquareBoxs([]);
-    }
-
-    return () => clearInterval(timeLeftRef.current);
-  }, [timeLeft, setNumOfWrongValidation, setImageSrc, ref, setSquareShapePostion, startInterval, setAllCaptchaSquareBoxs]);
-
-  const minuteLeft = Math.floor(timeLeft / 60);
-  const secondsLeft = Math.floor(timeLeft % 60);
+  const { secondsLeft, minuteLeft } = useBlockCountdown({ props, ref });
 
   const formatTime = (time: number) => {
     return time < 10 ? `0${time}` : time;
@@ -42,9 +16,9 @@ const BlockedError = forwardRef<number | undefined, IBlockedErrorProps>((props, 
           You're blocked for {formatTime(minuteLeft)} miniute {formatTime(secondsLeft)} seconds
         </h1>
 
-        <button className="px-10 py-2 bg-blue-600/80 text-xl text-white font-serif rounded-lg active:scale-[0.90] active:transition-all duration-500">
+        {/* <button className="px-10 py-2 bg-blue-600/80 text-xl text-white font-serif rounded-lg active:scale-[0.90] active:transition-all duration-500">
           Start Over
-        </button>
+        </button> */}
       </div>
     </section>
   );
---
+++ src/helper/useBlockCountdown.tsx
@@ -0,0 +1,37 @@
+import { ForwardedRef, MutableRefObject, useEffect, useRef, useState } from "react";
+import { IBlockedErrorProps } from "../types";
+
+const useBlockCountdown = ({ props, ref }: { props: IBlockedErrorProps; ref: ForwardedRef<number | undefined> }) => {
+  const { setNumOfWrongValidation, setImageSrc, startInterval, setSquareShapePostion, setAllCaptchaSquareBoxs } = props;
+
+  const [timeLeft, setTimeLeft] = useState(3600);
+  const timeLeftRef = useRef<number | undefined>();
+
+  useEffect(() => {
+    timeLeftRef.current = setInterval(() => {
+      setTimeLeft((prevSeconds) => {
+        if (prevSeconds <= 0) {
+          clearInterval(timeLeftRef.current);
+          return 0;
+        }
+        return prevSeconds - 1;
+      });
+    }, 1000);
+
+    if (timeLeft === 0) {
+      setNumOfWrongValidation(0);
+      setImageSrc(null);
+      (ref as MutableRefObject<number | undefined>).current = startInterval(setSquareShapePostion);
+      setAllCaptchaSquareBoxs([]);
+    }
+
+    return () => clearInterval(timeLeftRef.current);
+  }, [timeLeft, setNumOfWrongValidation, setImageSrc, ref, setSquareShapePostion, startInterval, setAllCaptchaSquareBoxs]);
+
+  const minuteLeft = Math.floor(timeLeft / 60);
+  const secondsLeft = Math.floor(timeLeft % 60);
+
+  return { minuteLeft, secondsLeft };
+};
+
+export default useBlockCountdown;
